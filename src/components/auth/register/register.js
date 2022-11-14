91e724fb891a2833a848c06d7342fa8657aafe2f
Shay <shaybush93@gmail.com>
2022-11-14 12:07:03 +0200

    start working on register login page

--- src/components/auth/register/register.js
+++ src/components/auth/register/register.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import { useState } from 'react';
 import { useForm } from "react-hook-form"
 import { useNavigate } from 'react-router-dom';
 import { API_URL, doApiMethod } from '../../../services/service';
-import { Wrapper } from '../../style/wrappers/registerPage';
-import { Button } from './style';
+import { Wrapper, Button } from '../../style/wrappers/registerPage';
 const Register = () => {
     const nav = useNavigate();
     let { register, getValues, handleSubmit, formState: { errors } } = useForm();
     const regEmail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+    const [isRegister, setIsRegister] = useState(true)
     const onSub = (_dataBody) => {
         delete _dataBody.password2
         delete _dataBody.email2
@@ -32,81 +33,93 @@ const Register = () => {
             console.log(err.response);
         }
     }
+    const handleClick = () => {
+        setIsRegister(!isRegister)
+    }
     return (
         <Wrapper>
-            <div className="inside_box">  
+            <div className="inside_box">
                 <div className="left w-full md:w-1/3">
                     <div className="gmail text-red-300">Gmail</div>
                     <div className="facebook text-blue-500">Facebook</div>
                 </div>
                 <div className="right w-full md:w-2/3">
-                    <h1 className='text-center text-2xl mb-3'>Register</h1>
+                    <h1 className='text-center text-2xl mb-3'>{isRegister ? "Register" : "Login"}</h1>
                     <form onSubmit={handleSubmit(onSub)}>
-                        <div className="flex flex-wrap -mx-3 mb-2">
+                        <div className="flex flex-wrap -mx-3 mb-2" style={{ display: isRegister ? 'flex' : 'none' }}>
                             <div className="w-full md:w-1/2 px-3 mb-2 md:mb-0">
-                                <label className="block uppercase tracking-wide text-xs font-bold mb-2" >
+                                <label>
                                     First Name
                                 </label>
-                                <input {...register('firstName', { required: true, minLength: 2, maxLength: 25 })} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-2 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-first-name" type="text" placeholder="Shimon" />
-                                {errors.firstName && <p className="text-red-500 text-xs italic">Enter valid name.</p>}
+                                <input {...register('firstName', { required: true, minLength: 2, maxLength: 25 })} type="text" placeholder="Shimon" />
+                                {errors.firstName && <small>Enter valid name.</small>}
                             </div>
                             <div className="w-full md:w-1/2 px-3">
-                                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" >
+                                <label >
                                     Last Name
                                 </label>
-                                <input {...register('lastName', { required: true, minLength: 2, maxLength: 25 })} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-2 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Doe" />
-                                {errors.lastName && <p className="text-red-500 text-xs italic">Enter valid last name.</p>}
+                                <input {...register('lastName', { required: true, minLength: 2, maxLength: 25 })} type="text" placeholder="Doe" />
+                                {errors.lastName && <small>Enter valid last name.</small>}
                             </div>
                         </div>
                         <div className="flex flex-wrap -mx-3 mb-2">
                             <div className="w-full px-3">
-                                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-1">
+                                <label>
                                     Email
                                 </label>
-                                <input {...register('email', { required: true, minLength: 2, maxLength: 25, pattern: regEmail })} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-2 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="email" placeholder="example@email.com" />
-                                {errors.email && <p className="text-red-500 text-xs italic">Please fill valid email.</p>}
+                                <input {...register('email', { required: true, minLength: 2, maxLength: 25, pattern: regEmail })} type="email" placeholder="example@email.com" />
+                                {errors.email && <small>Please fill valid email.</small>}
                             </div>
                         </div>
-                        <div className="flex flex-wrap -mx-3 mb-2">
+                        <div className="flex flex-wrap -mx-3 mb-2" style={{ display: isRegister ? 'flex' : 'none' }}>
                             <div className="w-full px-3">
-                                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                                <label>
                                     Confirm Email
                                 </label>
-                                <input {...register('email2', { required: true, validate: (value) => { return value === getValues('email') } })} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-2 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="email" placeholder="example@email.com" />
-                                {errors.email2 && <p className="text-red-500 text-xs italic">Email not match.</p>}
+                                <input {...register('email2', { required: true, validate: (value) => { return value === getValues('email') } })} type="email" placeholder="example@email.com" />
+                                {errors.email2 && <small>Email not match.</small>}
                             </div>
                         </div>
                         <div className="flex flex-wrap -mx-3 mb-2">
-                            <div className="w-full md:w-1/2 px-3">
-                                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                            <div className={isRegister ? "w-full md:w-1/2 px-3" : " w-full px-3"}>
+                                <label>
                                     Password
                                 </label>
-                                <input {...register("password", { required: true, minLength: 6, maxLength: 25 })} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-2 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="password" placeholder="******************" />
-                                {errors.password && <p className="text-red-500 text-xs italic">Please fill out this field.</p>}
+                                <input {...register("password", { required: true, minLength: 6, maxLength: 25 })} type="password" placeholder="******************" />
+                                {errors.password && <small>Please fill out this field.</small>}
                             </div>
-                            <div className="w-full md:w-1/2 px-3 ">
-                                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                            <div className="w-full md:w-1/2 px-3" style={{ display: isRegister ? 'block' : 'none' }}>
+                                <label>
                                     Confirm Password
                                 </label>
-                                <input {...register('password2', { required: true, validate: (value) => { return value === getValues('password') } })} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-2 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="password" placeholder="******************" />
-                                {errors.password2 && <p className="text-red-500 text-xs italic">Password dont match.</p>}
+                                <input {...register('password2', { required: true, validate: (value) => { return value === getValues('password') } })} type="password" placeholder="******************" />
+                                {errors.password2 && <small>Password dont match.</small>}
                             </div>
                         </div>
-
-
-                        <div className="relative mb-2">
-                            <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-                                <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
+                        <div className="flex flex-wrap mb-2 -mx-3" style={{ display: isRegister ? 'flex' : 'none' }}>
+                            <div className="relative w-1/3 px-2">
+                                <label>
+                                    Birthdate
+                                </label>
+                                <div className="flex absolute bottom-3 items-center pl-3 pointer-events-none">
+                                    <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
+                                </div>
+                                <input datepicker type="text" className="datepicker-input" placeholder="Select date" />
                             </div>
-                            <input datepicker="" type="text" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 datepicker-input" placeholder="Select date" />
+                            <div className="w-1/2 md:w-1/3 px-2 mb-2 md:mb-0">
+                                <label >
+                                    Phone
+                                </label>
+                                <input {...register('firstName', { required: true, minLength: 2, maxLength: 25 })} type="text" placeholder="Shimon" />
+                                {errors.firstName && <small>Enter valid name.</small>}
+                            </div>
                         </div>
                         <Button>
                             <button>Submit</button>
                         </Button>
-                        <span>Already a member ? </span>
+                        <span>{isRegister ? "Already a member ?" : "Not a member yet ?"} <button onClick={handleClick} className='underline text-blue-400 hover:text-blue-700'>click here</button> </span>
                     </form>
                 </div>
-
             </div>
         </Wrapper>
     )
--- src/components/style/wrappers/registerPage.js
+++ src/components/style/wrappers/registerPage.js
@@ -27,7 +27,63 @@ export const Wrapper = styled.div`
     }   
     .right{
         padding: 12px 36px 0px 0px;
+        input{
+            display: block;
+            width: 100%;
+            background: var(--grey-100);
+            color: var(--grey-700);
+            border: 1px solid var(--grey-200);
+            border-radius: 8px;
+            padding: 8px 32px;
+            margin-bottom: 2px;
+            &:focus{
+                outline: none;
+                background: var(--white);
+                border: 1px solid var(--grey-500);
+            }
+        }
+        label{
+            /* block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2 */
+            display: block;
+            color: var(--grey-700);
+            font-size: x-small;
+            font-weight: bold;
+            margin-bottom: 12px;
+            text-transform: uppercase;
+        }
+        small{
+            /* text-red-500 text-xs italic */
+            color: var(--red);
+            margin: 0;
+            padding: 0;
+            font-size: x-small;
+            font-style: italic;
+        }
     }
-    input{
+
+`
+
+export const Button = styled.div`
+    display: flex;
+    justify-content: center;
+    text-align: center;
+    margin-top:24px;
+    button{
+      display: flex;
+      justify-content: center;
+      padding: 8px 16px;
+      border-radius: 4px;
+      font-size: 1em;
+      background-color: #2d80c9;
+      color: white;
+      border: none;
+      cursor: pointer;
+      transition: all 0.2s ease-in-out;
+      &:hover{
+        background-color: #024580;
+        }
+      &:active{
+        transform: translateY(20deg)
+      }
     }
 `
