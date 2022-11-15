9b3fe281e8522c922147a0714132970b749060ec
shimislm <shimislm5@gmail.com>
2022-11-15 14:08:12 +0200

    login/reg ui changes

--- src/layoutAdmin/header/header.js
+++ src/layoutAdmin/header/header.js
@@ -55,6 +55,15 @@ const Header = () => {
               >
                 Users
               </Link>
+
+            </li>
+            <li>
+            <Link
+                to="/admin/categories"
+                className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              >
+                Categories
+              </Link>
             </li>
           </ul>
         </div>
