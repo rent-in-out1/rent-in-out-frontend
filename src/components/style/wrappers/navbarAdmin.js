b854495c9a5f9dcfde770d24fd7da99913da6eed
Shay <shaybush93@gmail.com>
2022-11-27 22:01:03 +0200

    last thing - navigate the side bar to the pages

--- src/components/style/wrappers/navbarAdmin.js
+++ src/components/style/wrappers/navbarAdmin.js
@@ -101,7 +101,14 @@ export const Wrapper = styled.div`
         }
     }
     }
-
+    .dropdown{
+        li{
+            transition: 1s ease;
+        }
+        li:hover{
+            background: var(--grey-300);
+        }
+    }
 
 `
 export const Logo = styled.div`
--- src/pages/client/dashboard/dashboard.js
+++ src/pages/client/dashboard/dashboard.js
@@ -2,16 +2,19 @@ import React from 'react'
 import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
+import SideBar from '../../../components/sideBar'
+import Main from '../../../components/main'
 const Dashboard = () => {
   const nav = useNavigate();
   const user = useSelector(state => state.userSlice.user)
   useEffect(() => {
-    if(user?.role === "admin") nav("/admin")
+    if (user?.role === "admin") nav("/admin")
   }, [user])
   return (
-    <div>
-
-    </div>
+    <div className='flex'>
+        <SideBar/>
+        <Main/>
+      </div>
   )
 }
 
