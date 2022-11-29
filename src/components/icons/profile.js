a7382155208e420960e308f9fb12fc3027a2ff79
Shay <shaybush93@gmail.com>
2022-11-29 12:03:25 +0200

    side bar icons and styled components done

---
+++ src/components/icons/profile.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Profile = ({ color = "#6B7280", width = "24px", height = "24px" }) => {
+    return (
+        <svg aria-hidden="true" className="transition duration-75" width={width} height={height} fill={color} viewBox="0 0 20 20">
+            <path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd"></path>
+        </svg>
+    )
+}
+
+export default Profile
