4b0b11673a10bc111504d81830ba4b8d5a8c3407
ido1239 <ido12301f@gmail.com>
2022-11-29 12:46:09 +0200

    start work on card user

---
+++ src/pages/client/userSearch/userSearch.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import UserCard from '../../../components/userCard'
+
+const UserSearch = () => {
+  return (
+
+    <div>
+        <input/>
+        <UserCard/>
+    </div>
+  )
+}
+
+export default UserSearch
