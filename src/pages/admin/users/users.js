253e193a2e706721f9cfb72e84099b030b67cffe
Shay <shaybush93@gmail.com>
2022-11-30 16:43:52 +0200

    start working on card display

--- src/pages/admin/users/users.js
+++ src/pages/admin/users/users.js
@@ -4,15 +4,17 @@ import { Wrapper } from "../../../components/style/wrappers/table";
 import SingleUser from "./singleUser";
 import Controllers from "../../../components/controllers/controllers";
 import { useScroll } from "./../../../hooks/useScroll";
+import Loader from "../../../components/loaderImg/loaderImg";
 const Users = () => {
   const [users, setUsers] = useState([]);
   const [search, setSearch] = useState("");
   const [option, setOption] = useState();
+  const [isLoading, setIsLoading] = useState(false);
   //lazy loading users
   const [firstLoad, setFirstLoad] = useState(true);
   const [endScreen, endScreenEnd] = useScroll(900);
   const [page, setPage] = useState(1);
-  
+
   const [isChange, setIsChange] = useState(false);
   const options = [
     { name: "Role", value: "role" },
@@ -26,10 +28,10 @@ const Users = () => {
   }, [isChange, option, search, page]);
 
   useEffect(() => {
-    const count =async ()=>{
+    const count = async () => {
       if (!firstLoad && endScreen) {
-        let {data} = await doGetApiMethod("/users/countUsers");
-        if(page > (data/10)) setPage(data)
+        let { data } = await doGetApiMethod("/users/countUsers");
+        if (page +1 > (data / 10)) setPage(data / 10)
         else setPage(page + 1);
       }
       setFirstLoad(false);
@@ -38,10 +40,23 @@ const Users = () => {
   }, [endScreen]);
 
   const getAllUsers = async () => {
-    if(isChange) setUsers([])
-    let url = `/users/search/?s=${search}&sort=${option}&page=${page}`;
-    let {data} = await doGetApiMethod(url);
-    setUsers([...users ,...data]);
+    if (search) {
+      setIsLoading(true)
+      setPage(1);
+      setUsers([])
+      let url = `/users/search/?s=${search}&sort=${option}&page=${page}`;
+      let { data } = await doGetApiMethod(url);
+      setIsLoading(false)
+      console.log(page)
+      setUsers(data);
+    }
+    else {
+      setIsLoading(true)
+      let url = `/users/search/?s=${search}&sort=${option}&page=${page}`;
+      let { data } = await doGetApiMethod(url);
+      setIsLoading(false)
+      setUsers([...users, ...data]);
+    }
     setIsChange(false);
     endScreenEnd();
   };
@@ -69,7 +84,7 @@ const Users = () => {
               <th>delete</th>
             </tr>
           </thead>
-          <tbody>
+          <tbody id="tbody">
             {users.length > 0 && users?.map((user) => {
               return (
                 <SingleUser
@@ -79,9 +94,9 @@ const Users = () => {
                 />
               );
             })}
+            {isLoading && <div className="flex justify-center w-full"><Loader/></div>}
           </tbody>
         </table>
-            {/* {endScreen && <h3>loading...</h3>} */}
       </div>
     </Wrapper>
   );
