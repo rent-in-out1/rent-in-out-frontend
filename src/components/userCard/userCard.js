76b4440c3f278f26bc7cfe94ecdecdc831d8f02d
ido1239 <ido12301f@gmail.com>
2022-11-30 16:10:25 +0200

    start user serach

--- src/components/userCard/userCard.js
+++ src/components/userCard/userCard.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
-const UserCard = () => {
+const UserCard = ({item}) => {
+   
   return (
+
     <div className='w-1/2 mt-3'>
         <div className='p-2'>
-            
+            {item.fullName.firstName}
         </div>
         
     </div>
--- src/pages/client/userSearch/userSearch.js
+++ src/pages/client/userSearch/userSearch.js
@@ -1,13 +1,50 @@
 import React from 'react'
+import { useState,useRef, useEffect } from 'react'
+import {  useSearchParams, useNavigate } from 'react-router-dom';
 import UserCard from '../../../components/userCard'
+import {doGetApiMethod, errorHandler } from "../../../services/service";
 
 const UserSearch = () => {
+  const nav = useNavigate();
+  const [query] = useSearchParams();
+  const inpRef = useRef();
+  const [ar,setAr] = useState([]);
+
+  useEffect(() => {
+    serachUser();
+  }, [query.get("s")]);
+
+
+  const serachUser = async () => {
+    let searchQ = query.get("s") || query.get("ido");
+    const url = "/users/search?s=" +searchQ;
+    try {
+      const {data}  = await doGetApiMethod(url);
+      console.log(data);
+      setAr(data);
+    } catch (err) {
+      errorHandler(err.response.data.msg)
+    }
+  }
+
   return (
 
     <div>
-        
-        <input/>
-        <UserCard/>
+        <div>
+        <input  onChange={serachUser} ref= {inpRef} onKeyDown={(e)=>{
+               if(e.key == "Enter"){
+                serachUser()
+              }
+        }} type="text" placeholder= "search users"/>
+        </div>
+        <div>
+        {ar?.map(item =>{
+          return(
+            <UserCard key={item._id} item={item}/>
+          )
+        })}
+      </div>
+       
     </div>
   )
 }
