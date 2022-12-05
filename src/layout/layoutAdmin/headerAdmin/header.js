790e9465a946521cce01c15fb71592e97ab55835
shimislm <shimislm5@gmail.com>
2022-12-05 12:23:34 +0200

    search changes

--- src/layout/layoutAdmin/headerAdmin/header.js
+++ src/layout/layoutAdmin/headerAdmin/header.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { onRegisterToggle, onSearchToggle } from '../../../redux/features/toggleSlice';
+import {
+  onRegisterToggle,
+  onSearchToggle,
+} from "../../../redux/features/toggleSlice";
 import { Logo, Wrapper } from "../../../components/style/wrappers/navbarAdmin";
 import { useSelector, useDispatch } from "react-redux";
 import { onLogout } from "../../../redux/features/userSlice";
@@ -12,12 +15,12 @@ import Home from "../../../components/icons/home";
 import SignIn from "../../../components/icons/signIn";
 import SignOut from "../../../components/icons/signOut";
 import Bell from "../../../components/icons/bell";
-import Search from './../../../components/icons/search';
+import Search from "./../../../components/icons/search";
 const Header = () => {
   const nav = useNavigate();
   const dispatch = useDispatch();
-  const isLogin = useSelector(state => state.userSlice?.user !== null)
-  const user = useSelector(state => state.userSlice.user)
+  const isLogin = useSelector((state) => state.userSlice?.user !== null);
+  const user = useSelector((state) => state.userSlice.user);
   const [isOpen, setIsOpen] = useState(false);
   return (
     <Wrapper>
@@ -47,7 +50,11 @@ const Header = () => {
               </li>
             </ul>
           </nav>
-          <div className="relative avatar" onMouseOver={() => setIsOpen(true)} onClick={() => setIsOpen(true)}>
+          <div
+            className="relative avatar"
+            onMouseOver={() => setIsOpen(true)}
+            onClick={() => setIsOpen(true)}
+          >
             <img
               className="rounded-full"
               src={
@@ -58,68 +65,112 @@ const Header = () => {
               alt=""
             />
             <span
-              className={`${isLogin ? "bg-green-400" : "bg-red-400"
-                } bottom-0 left-7 absolute  w-3.5 h-3.5 border-2 border-white dark:border-gray-800 rounded-full`}
+              className={`${
+                isLogin ? "bg-green-400" : "bg-red-400"
+              } bottom-0 left-7 absolute  w-3.5 h-3.5 border-2 border-white dark:border-gray-800 rounded-full`}
             ></span>
           </div>
         </div>
       </section>
-      {
-        isOpen &&
-        <ul onMouseLeave={() => setIsOpen(false)} className='absolute shadow dropdown z-50 bg-white w-full rounded right-0 -top-15 md:w-1/4 md:-bottom-30'>
-          <li onClick={() => {
-            setIsOpen(false)
-            nav("/admin")
-          }} className={`w-full p-2 rounded transition ease-in-out delay-150 cursor-pointer hover:bg-blue-200`}>
-            <div className='flex justify-between items-center'> <p>Home</p> <Home /></div>
-            </li>
-          <li onClick={()=>{
-                setIsOpen(false)
-                nav("/admin/users")
-                }} className={`w-full p-2 rounded transition ease-in-out delay-150 cursor-pointer hover:bg-blue-200`}>
-            <div className='flex justify-between items-center'> <p>Users</p> <Profile color="black"/></div>
-            </li>
-          <li onClick={()=>{
-                setIsOpen(false)
-                nav("/admin/posts")
-                }} className={`w-full p-2 rounded transition ease-in-out delay-150 cursor-pointer hover:bg-blue-200`}>
-                  <div className='flex justify-between items-center'> <p>Posts</p> <Posts color="black" /></div>
-            </li>
-          <li onClick={()=>{
-                setIsOpen(false)
-                nav("/admin/categories")
-                }} className={`w-full p-2 rounded transition ease-in-out delay-150 cursor-pointer hover:bg-blue-200`}>
-                  <div className='flex justify-between items-center'> <p>Categories</p> <Categories /></div>
-            </li>
-          <li onClick={()=>{
-                setIsOpen(false)
-                nav("/admin/profile")
-                }} className={`w-full p-2 rounded transition ease-in-out delay-150 cursor-pointer hover:bg-blue-200`}>
-                  <div className='flex justify-between items-center'> <p>Profile</p> <Users /></div>
-            </li>
-            <li
-             onClick={()=> dispatch(onSearchToggle())}
-             className={`w-full p-2 rounded transition ease-in-out delay-150 cursor-pointer hover:bg-blue-200`}>
-            <Link className='flex justify-between items-center'>
+      {isOpen && (
+        <ul
+          onMouseLeave={() => setIsOpen(false)}
+          className="absolute shadow dropdown z-50 bg-white w-full rounded right-0 -top-15 md:w-1/4 md:-bottom-30"
+        >
+          <li
+            onClick={() => {
+              setIsOpen(false);
+              nav("/admin");
+            }}
+            className={`w-full p-2 rounded transition ease-in-out delay-150 cursor-pointer hover:bg-blue-200`}
+          >
+            <div className="flex justify-between items-center">
+              {" "}
+              <p>Home</p> <Home />
+            </div>
+          </li>
+          <li
+            onClick={() => {
+              setIsOpen(false);
+              nav("/admin/users");
+            }}
+            className={`w-full p-2 rounded transition ease-in-out delay-150 cursor-pointer hover:bg-blue-200`}
+          >
+            <div className="flex justify-between items-center">
+              {" "}
+              <p>Users</p> <Profile color="black" />
+            </div>
+          </li>
+          <li
+            onClick={() => {
+              setIsOpen(false);
+              nav("/admin/posts");
+            }}
+            className={`w-full p-2 rounded transition ease-in-out delay-150 cursor-pointer hover:bg-blue-200`}
+          >
+            <div className="flex justify-between items-center">
+              {" "}
+              <p>Posts</p> <Posts color="black" />
+            </div>
+          </li>
+          <li
+            onClick={() => {
+              setIsOpen(false);
+              nav("/admin/categories");
+            }}
+            className={`w-full p-2 rounded transition ease-in-out delay-150 cursor-pointer hover:bg-blue-200`}
+          >
+            <div className="flex justify-between items-center">
+              {" "}
+              <p>Categories</p> <Categories />
+            </div>
+          </li>
+          <li
+            onClick={() => {
+              setIsOpen(false);
+              nav("/admin/profile");
+            }}
+            className={`w-full p-2 rounded transition ease-in-out delay-150 cursor-pointer hover:bg-blue-200`}
+          >
+            <div className="flex justify-between items-center">
+              <p>Profile</p> <Users />
+            </div>
+          </li>
+          <li
+            onClick={() => dispatch(onSearchToggle())}
+            className={`w-full p-2 rounded transition ease-in-out delay-150 cursor-pointer hover:bg-blue-200`}
+          >
+            <Link className="flex justify-between items-center">
               <span>Search</span>
               <Search />
             </Link>
           </li>
-          <li onClick={() => {
-            if (isLogin) {
-              localStorage.removeItem('token')
-              nav("/")
-              dispatch(onLogout())
-            }
-            else {
-              dispatch(onRegisterToggle())
-              setIsOpen(false)
-            }
-          }} className={`w-full p-2 rounded`}>
-            {isLogin ? <div className='flex justify-between items-center cursor-pointer'> <p>Signout</p> <SignOut color="black" /></div> :
-              <div className='flex justify-between items-center'><p>Signin</p> <SignIn color="black"/></div>}</li>
+          <li
+            onClick={() => {
+              if (isLogin) {
+                localStorage.removeItem("token");
+                nav("/");
+                dispatch(onLogout());
+              } else {
+                dispatch(onRegisterToggle());
+                setIsOpen(false);
+              }
+            }}
+            className={`w-full p-2 rounded`}
+          >
+            {isLogin ? (
+              <div className="flex justify-between items-center cursor-pointer">
+                {" "}
+                <p>Signout</p> <SignOut color="black" />
+              </div>
+            ) : (
+              <div className="flex justify-between items-center">
+                <p>Signin</p> <SignIn color="black" />
+              </div>
+            )}
+          </li>
         </ul>
-      }
+      )}
     </Wrapper>
   );
 };
--- src/layout/layoutUser/header/header.js
+++ src/layout/layoutUser/header/header.js
@@ -1,31 +1,34 @@
-
-import React from 'react'
-import { Link,useNavigate } from 'react-router-dom'
-import { Logo, Wrapper } from '../../../components/style/wrappers/navbarUser';
-import { useSelector, useDispatch } from "react-redux"
-import { useState } from 'react';
-import {  onLogout, onRegisterShow, onSearchToggle } from '../../../redux/features/toggleSlice';
-import { API_URL_CLIENT } from '../../../services/service';
-import Search from '../../../components/icons/search';
-import Dashboard from '../../../components/icons/dashboard';
-import Profile from '../../../components/icons/profile';
-import Bell from '../../../components/icons/bell';
-import Settings from '../../../components/icons/settings';
-import Inbox from '../../../components/icons/inbox';
-import SignIn from '../../../components/icons/signIn';
-import SignOut from '../../../components/icons/signOut';
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Logo, Wrapper } from "../../../components/style/wrappers/navbarUser";
+import { useSelector, useDispatch } from "react-redux";
+import { useState } from "react";
+import {
+  onLogout,
+  onRegisterShow,
+  onSearchToggle,
+} from "../../../redux/features/toggleSlice";
+import { API_URL_CLIENT } from "../../../services/service";
+import Search from "../../../components/icons/search";
+import Dashboard from "../../../components/icons/dashboard";
+import Profile from "../../../components/icons/profile";
+import Bell from "../../../components/icons/bell";
+import Settings from "../../../components/icons/settings";
+import Inbox from "../../../components/icons/inbox";
+import SignIn from "../../../components/icons/signIn";
+import SignOut from "../../../components/icons/signOut";
 
 const Header = () => {
   const nav = useNavigate();
   const dispatch = useDispatch();
-  const isLogin = useSelector(state => state.userSlice?.user !== null)
-  const { user } = useSelector(state => state.userSlice)
+  const isLogin = useSelector((state) => state.userSlice?.user !== null);
+  const { user } = useSelector((state) => state.userSlice);
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <Wrapper>
       <section>
-        <div className='left flex flex-wrap'>
+        <div className="left flex flex-wrap">
           <Link to={"/"}>
             <Logo>
               <img src="./img/LOGO.png" alt="logo" />
@@ -33,34 +36,52 @@ const Header = () => {
             </Logo>
           </Link>
           <div className="search">
-            <input type="text" placeholder='Search...' className='border-transparent focus:border-transparent focus:ring-0' />
+            <input
+              type="text"
+              placeholder="Search..."
+              className="border-transparent focus:border-transparent focus:ring-0"
+            />
             <div className="icon">
-              <Search color='#333' width='16' height='16'/>
+              <Search color="#333" width="16" height="16" />
             </div>
           </div>
         </div>
-        <div className='right'>
+        <div className="right">
           <nav>
-            {isLogin &&
+            {isLogin && (
               <ul>
                 <li>
-                  <button type="button" className="inline-flex relative items-center p-3 text-sm  text-center">
-                    <Inbox color='black' width='20' height='20' />
+                  <button
+                    type="button"
+                    className="inline-flex relative items-center p-3 text-sm  text-center"
+                  >
+                    <Inbox color="black" width="20" height="20" />
                     <span className="sr-only">Notifications</span>
-                    <div className="z-10 inline-flex absolute -top-1 -right-2 justify-center items-center w-5 h-5 text-xs font-bold text-white bg-red-500 rounded-full">1</div>
+                    <div className="z-10 inline-flex absolute -top-1 -right-2 justify-center items-center w-5 h-5 text-xs font-bold text-white bg-red-500 rounded-full">
+                      1
+                    </div>
                   </button>
                 </li>
                 <li>
-                  <button type="button" className="inline-flex relative items-center p-3 text-sm  text-center">
-                    <Bell/>
+                  <button
+                    type="button"
+                    className="inline-flex relative items-center p-3 text-sm  text-center"
+                  >
+                    <Bell />
                     <span className="sr-only">Notifications</span>
-                    <div className="z-10 inline-flex absolute -top-1 -right-2 justify-center items-center w-5 h-5 text-xs font-bold text-white bg-red-500 rounded-full">2</div>
+                    <div className="z-10 inline-flex absolute -top-1 -right-2 justify-center items-center w-5 h-5 text-xs font-bold text-white bg-red-500 rounded-full">
+                      2
+                    </div>
                   </button>
                 </li>
               </ul>
-            }
+            )}
           </nav>
-          <div className="relative avatar" onMouseOver={() => setIsOpen(true)} onClick={() => setIsOpen(true)}>
+          <div
+            className="relative avatar"
+            onMouseOver={() => setIsOpen(true)}
+            onClick={() => setIsOpen(true)}
+          >
             <img
               className="rounded-full"
               src={
@@ -70,62 +91,96 @@ const Header = () => {
               }
               alt=""
             />
-            <span className={`${isLogin ? "bg-green-400" : "bg-red-400"} bottom-0 left-7 absolute  w-3.5 h-3.5 border-2 border-white dark:border-gray-800 rounded-full`}></span>
-
+            <span
+              className={`${
+                isLogin ? "bg-green-400" : "bg-red-400"
+              } bottom-0 left-7 absolute  w-3.5 h-3.5 border-2 border-white dark:border-gray-800 rounded-full`}
+            ></span>
           </div>
         </div>
       </section>
-      {
-        isOpen &&
-        <ul  onMouseLeave={() => setIsOpen(false)} className='absolute dropdown transition shadow bg-white z-50 w-full rounded right-0 -top-15 md:w-1/4 md:-bottom-30'>
-          {isLogin &&
+      {isOpen && (
+        <ul
+          onMouseLeave={() => setIsOpen(false)}
+          className="absolute dropdown transition shadow bg-white z-50 w-full rounded right-0 -top-15 md:w-1/4 md:-bottom-30"
+        >
+          {isLogin && (
             <React.Fragment>
-              <li onClick={()=>{
-                setIsOpen(false)
-                nav("/")
-                }} className={`w-full p-2 rounded transition ease-in-out delay-150 cursor-pointer`}>
-                <div className='flex justify-between items-center'><p>Home</p> <Dashboard color='black'/></div>
-              </li>
-              <li onClick={()=>{
-                setIsOpen(false)
-                nav("/profile")
-                }} className={`w-full p-2 rounded transition ease-in-out delay-150 cursor-pointer`}>
-                <div className='flex justify-between items-center'> <p>Profile</p> <Profile color='black' /></div>
-              </li>
-              <li onClick={()=>{
-                setIsOpen(false)
-                nav("/profile1")
-                }} className={`w-full p-2 rounded transition ease-in-out delay-150 cursor-pointer`}>
-                <div className='flex justify-between items-center'> <p>Settings</p> <Settings /></div>
+              <li
+                onClick={() => {
+                  setIsOpen(false);
+                  nav("/");
+                }}
+                className={`w-full p-2 rounded transition ease-in-out delay-150 cursor-pointer`}
+              >
+                <div className="flex justify-between items-center">
+                  <p>Home</p> <Dashboard color="black" />
+                </div>
               </li>
               <li
-             onClick={()=> dispatch(onSearchToggle())}
-             className={`w-full p-2 rounded transition ease-in-out delay-150 cursor-pointer hover:bg-blue-200`}>
-            <Link className='flex justify-between items-center'>
+                onClick={() => {
+                  setIsOpen(false);
+                  nav("/profile");
+                }}
+                className={`w-full p-2 rounded transition ease-in-out delay-150 cursor-pointer`}
+              >
+                <div className="flex justify-between items-center">
+                  {" "}
+                  <p>Profile</p> <Profile color="black" />
+                </div>
+              </li>
+              <li
+                onClick={() => {
+                  setIsOpen(false);
+                  nav("/profile1");
+                }}
+                className={`w-full p-2 rounded transition ease-in-out delay-150 cursor-pointer`}
+              >
+                <div className="flex justify-between items-center">
+                  {" "}
+                  <p>Settings</p> <Settings />
+                </div>
+              </li>
+            </React.Fragment>
+          )}
+          <li
+            onClick={() => dispatch(onSearchToggle())}
+            className={`w-full p-2 rounded transition ease-in-out delay-150 cursor-pointer hover:bg-blue-200`}
+          >
+            <Link className="flex justify-between items-center">
               <span>Search</span>
               <Search />
             </Link>
           </li>
-            </React.Fragment>
-          }
-          <li onClick={() => {
-            if (isLogin) {
-              localStorage.removeItem('token')
-              window.open(API_URL_CLIENT, "_self")
-              dispatch(onLogout())
-              setIsOpen(false)
-            }
-            else {
-              // nav("/register")
-              dispatch(onRegisterShow())
-              setIsOpen(false)
-            }
-          }} className={`w-full p-2 rounded cursor-pointer`}>
-            {isLogin ? <div className='flex justify-between items-center'> <p>Signout</p> <SignOut color='black' /></div> :
-              <div className='flex justify-between items-center'><p>Signin</p> <SignIn /></div>}</li>
+          <li
+            onClick={() => {
+              if (isLogin) {
+                localStorage.removeItem("token");
+                window.open(API_URL_CLIENT, "_self");
+                dispatch(onLogout());
+                setIsOpen(false);
+              } else {
+                // nav("/register")
+                dispatch(onRegisterShow());
+                setIsOpen(false);
+              }
+            }}
+            className={`w-full p-2 rounded cursor-pointer`}
+          >
+            {isLogin ? (
+              <div className="flex justify-between items-center">
+                {" "}
+                <p>Signout</p> <SignOut color="black" />
+              </div>
+            ) : (
+              <div className="flex justify-between items-center">
+                <p>Signin</p> <SignIn />
+              </div>
+            )}
+          </li>
         </ul>
-      }
+      )}
     </Wrapper>
-  )
-}
-export default Header
+  );
+};
+export default Header;
