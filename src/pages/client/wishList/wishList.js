c131e058dc8401bbff42ce1fc446109658b71ad3
shimislm <shimislm5@gmail.com>
2022-12-10 17:30:16 +0200

    remove some unnececry fetures from wish list

--- src/pages/client/wishList/wishList.js
+++ src/pages/client/wishList/wishList.js
@@ -6,19 +6,6 @@ import { doGetApiMethod } from "../../../services/service";
 
 const WishList = () => {
   let { user } = useSelector((state) => state.userSlice);
-  const dispatch = useDispatch();
-  const [isChange, setIsChange] = useState(false);
-  const [wishListArr, setWishList] = useState(false);
-  useEffect(() => {
-    getUserInfo()
-  }, [isChange]);
-  const getUserInfo = async () => {
-    let url = `/users/info/${user._id}`
-    const {data} =  await doGetApiMethod(url)
-    setWishList(data.userInfo.wishList)
-    dispatch(onLogin(data.userInfo))
-    setIsChange(false)
-  }
   return (
     <main className="w-full min-h-screen p-1 md:p-3 bg-gray-100 text-center justify-center">
       <div className="flex flex-wrap">
