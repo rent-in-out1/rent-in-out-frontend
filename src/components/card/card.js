3e43f310ec127287e0c1d8844707d8be85767e49
Shay <shaybush93@gmail.com>
2022-12-11 11:38:31 +0200

    There is an option to send message to seller

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -200,13 +200,10 @@ const Card = ({ post }) => {
               </span>
               <span className="text-xs capitalize text-gray-400">per day</span>
             </div>
-            <Link
-              to={"/"}
-              className="text-white justify-center items-center flex bg-blue-400 hover:bg-blue-800 font-medium rounded-lg text-sm px-3 py-2 md:px-5 md:py-2.5"
-            >
+            <a href={`https://wa.me/+972${owner.phone}?text=Hello Shimon i saw your item ${post.title} from rentInOut. \n i would like to rent it !`} target={"_blank"} rel="noreferrer" className="text-white justify-center items-center flex bg-blue-400 hover:bg-blue-800 font-medium rounded-lg text-sm px-3 py-2 md:px-5 md:py-2.5">
               <p className="mr-2 text-xs capitalize lg:text-lg">Rent now</p>
               <Chat color="white" />
-            </Link>
+            </a>
           </div>
         </div>
       </div>
