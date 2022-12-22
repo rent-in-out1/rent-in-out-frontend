d0f98feb54c9961af9625f7792a8bfbdd622003e
shimislm <shimislm5@gmail.com>
2022-12-22 10:53:35 +0200

    add chat to admin layout

--- src/components/sideBarChat/sideBarChat.js
+++ src/components/sideBarChat/sideBarChat.js
@@ -9,7 +9,7 @@ const SideBarChat = () => {
         <div className='md:w-3/12 lg:w-2/12 p-2 fixed top-16 hidden md:flex'>
             <div className="overflow-y-auto py-4 w-full mt-4 px-2 bg-white shadow-xl rounded">
                 <div className='flex justify-between'>
-                    <h2 className='flex items-center'><span className='mr-1'>Chats</span> <span><Chat/></span></h2>
+                    <h2 className='flex items-center'><span className='mr-1'>Chats</span><span><Chat/></span></h2>
                     <span className='text-sm text-blue-400 cursor-pointer'>All</span>
                 </div>
                 {/* all users with last chat here */}
--- src/layout/layoutAdmin/layoutAdmin.js
+++ src/layout/layoutAdmin/layoutAdmin.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./headerAdmin";
-import Sidebar from '../../components/sideBar'
+import Sidebar from "../../components/sideBar";
+import SideBarChat from "../../components/sideBarChat/sideBarChat";
 
 const LayoutAdmin = () => {
   return (
-    <div className='bg-gray-100'>
-      <Header/>
+    <div className="bg-gray-100">
+      <Header />
       <Sidebar />
-      <div className='lg:w-9/12 ml-auto'>
-      <Outlet />
+      <div className="lg:w-9/12 ml-auto">
+        <Outlet />
+      </div>
+      <div className="hidden md:flex md:w-3/12 lg:w-2/12">
+        <SideBarChat />
       </div>
     </div>
   );
