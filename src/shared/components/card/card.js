acfb4b0853422c5824a9cb6a1595d97aecf09741
Shay Bushary <48178609+Shaybush@users.noreply.github.com>
2023-08-23 22:44:22 +0300

    Bugfix (#13)
    * bugfix issue resolved :bug:
    
    * fix loading
    
    * ren-44
    
    * fix guest actions updated packages
    
    * fix share bug
    
    * should fix like likes
    
    * fix card bug
    
    ---------
    
    Co-authored-by: Shay Bushary <shay.bushary@1touch.io>

--- src/shared/components/card/card.js
+++ src/shared/components/card/card.js
@@ -5,8 +5,8 @@ import Heart from "../../../assets/icons/heart";
 import { Wrapper } from "../../../assets/styles/wrappers/card";
 import { likePost, setIsChange } from "../../../redux/features/postsSlice";
 import {
-    onPostToggle,
-    onRegisterShow
+  onPostToggle,
+  onRegisterShow,
 } from "../../../redux/features/toggleSlice";
 import { updateWishList } from "../../../redux/features/userSlice";
 import { unitTimeToCreatedTimeHelper } from "../../../services/extra-services/extra-services";
@@ -15,98 +15,95 @@ import PostHeader from "../postHeader/postHeader";
 import RecentLikes from "../recentLikes/recentLikes";
 
 const Card = ({ post }) => {
-    const dispatch = useDispatch();
-    const { user } = useSelector((state) => state.userSlice);
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.userSlice);
+  
+  return (
+    <Wrapper>
+      <PostHeader post={post} />
 
+      {/* post image */}
+      <div
+        className="relative cursor-pointer"
+        onDoubleClick={() => {
+          !user
+            ? dispatch(onRegisterShow())
+            : dispatch(likePost({ id: post._id }));
+          if (post.creator_id._id !== user?._id) {
+            dispatch(updateWishList(post));
+          }
+          dispatch(setIsChange());
+        }}
+      >
+        <div className="overflow-hidden w-full postImg">
+          <img
+            className="w-full h-full object-cover"
+            src={post?.img[0]?.url}
+            alt="post"
+          />
+        </div>
 
-    return (
-        <Wrapper>
-            <PostHeader post={post} />
+        {/* like icon */}
+        <div
+          className="absolute top-0 right-2 p-2"
+          onClick={() => {
+            if (!user) dispatch(onRegisterShow());
+            else {
+              dispatch(likePost({ id: post._id }));
+            }
+            if (post.creator_id._id !== user?._id) {
+              dispatch(updateWishList(post));
+            }
+            dispatch(setIsChange());
+          }}
+        >
+          {post?.likes?.length > 0 &&
+          post?.likes?.some((like) => like._id === user?._id) > 0 ? (
+            <FillHeart color="red" width="20px" height={"20px"} />
+          ) : (
+            <Heart color="red" width="20px" height={"20px"} />
+          )}
+        </div>
+      </div>
 
-            {/* post image */}
-            <div
-                className="relative cursor-pointer"
-                onDoubleClick={() => {
-                    !user
-                        ? dispatch(onRegisterShow())
-                        : dispatch(likePost({ id: post._id }));
-                    if (post.creator_id._id !== user._id) {
-                        dispatch(updateWishList(post));
-                    }
-                    dispatch(setIsChange());
-                }}
-            >
-                <div className="overflow-hidden w-full postImg">
-                    <img
-                        className="w-full h-full object-cover"
-                        src={post?.img[0]?.url}
-                        alt="post"
-                    />
-                </div>
+      {/* card footer */}
+      <div
+        onClick={() => {
+          dispatch(onPostToggle(post));
+        }}
+        className="px-3 pt-3"
+      >
+        {/* post title */}
+        <h4 className="mb-1 capitalize font-semibold text-gray-900 cursor-pointer">
+          {post?.title}
+        </h4>
 
-                {/* like icon */}
-                <div
-                    className="absolute top-0 right-2 p-2"
-                    onClick={() => {
-                        if(!user) dispatch(onRegisterShow())
-                         else{
-                            dispatch(likePost({ id: post._id }))
-                        }  
-                        if (post.creator_id._id !== user._id) {
-                            dispatch(updateWishList(post));
-                        }
-                        dispatch(setIsChange());
-                    }}
-                >
-                    {post?.likes?.length > 0 && post?.likes?.some(like=> like._id===user._id) > 0 ? (
-                        <FillHeart color="red" width="20px" height={"20px"} />
-                    ) : (
-                        <Heart color="red" width="20px" height={"20px"} />
-                    )}
-                </div>
-            </div>
+        <div className="flex justify-between items-center py-1">
+          {/* top 3 likes */}
+          <RecentLikes key={post._id} likes={post.likes} />
 
-            {/* card footer */}
-            <div
-                onClick={() => {
-                    dispatch(onPostToggle(post));
-                }}
-                className="px-3 pt-3">
+          {/* post uploaded time */}
+          <span className="bg-gray-100 text-gray-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded lg:mr-2">
+            <Clock />
+            {unitTimeToCreatedTimeHelper(post.createdAt)}
+          </span>
+        </div>
 
-                {/* post title */}
-                <h4 className="mb-1 capitalize font-semibold text-gray-900 cursor-pointer">
-                    {post?.title}
-                </h4>
+        <div className="h-full">
+          <div className="flex items-center justify-center">
+            {/* post price */}
+            <span className="text-xl md:text-2xl font-bold py-1 text-gray-900 mr-1">
+              {post?.price}$
+            </span>
 
-                <div className="flex justify-between items-center py-1">
-
-                    {/* top 3 likes */}
-                    <RecentLikes key={post._id} likes={post.likes} />
-
-                    {/* post uploaded time */}
-                    <span
-                        className="bg-gray-100 text-gray-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded lg:mr-2">
-                        <Clock />
-                        {unitTimeToCreatedTimeHelper(post.createdAt)}
-                    </span>
-                </div>
-
-                <div className="h-full">
-                    <div className="flex items-center justify-center">
-
-                        {/* post price */}
-                        <span className="text-xl md:text-2xl font-bold py-1 text-gray-900 mr-1">
-                            {post?.price}$
-                        </span>
-
-                        {/* price for day/week/month */}
-                        <span className="text-xs capitalize text-gray-400">per day</span>
-                    </div>
-                    <ChatAndWhatsup post={post} user={user} owner={post.creator_id} />
-                </div>
-            </div>
-        </Wrapper>
-    );
+            {/* price for day/week/month */}
+            <span className="text-xs capitalize text-gray-400">per day</span>
+          </div>
+          <ChatAndWhatsup post={post} user={user} owner={post.creator_id} />
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Card;
