84bea75ad376397b384578518e2b577e5fa3edfe
Shay Bushary <48178609+Shaybush@users.noreply.github.com>
2023-09-01 10:31:14 +0300

    Bugfix (#26)
    * bugfix issue resolved :bug:
    
    * fix loading
    
    * ren-44
    
    * fix guest actions updated packages
    
    * fix share bug
    
    * should fix like likes
    
    * fix card bug
    
    * fix loading cards and side bar
    
    * change to production
    
    * fix package json
    
    * moving to production
    
    * adding delete post from redux
    
    * change chat design
    
    * fix warning
    
    * ren-57 - fixed
    
    * ren-59 - align structure
    
    * ren-59 - align strucutre
    
    * testing deploy with netlify
    
    * update to production
    
    ---------
    
    Co-authored-by: Shay Bushary <shay.bushary@1touch.io>

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Provider } from 'react-redux';
 import AppRoutes from "./appRoutes";
 import "./index.css";
 import store from "./redux/store";
-
+// test
 function App() {
     return (
         <Provider store={store}>
