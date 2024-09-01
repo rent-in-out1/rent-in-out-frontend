1c0e29e129b8ce36addb9a6287bcf2c1b955ec7a
raztz15 <54091812+raztz15@users.noreply.github.com>
2024-09-01 21:38:42 +0300

    Migrate hooks folder to TS (#51)

--- src/hooks/useScroll.ts
+++ src/hooks/useScroll.ts
@@ -1,23 +1,22 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
-export function useScroll(offsetY = 0) {
+export function useScroll(offsetY: number = 0) {
     const [endScreen, setEndScreen] = useState(false);
 
     useEffect(() => {
-        window.addEventListener("scroll", onScroll);
+        window.addEventListener('scroll', onScroll);
         return () => {
-            window.removeEventListener("scroll", onScroll);
+            window.removeEventListener('scroll', onScroll);
         };
-        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     const onScroll = () => {
         // return window height in pexels
-        let windowHeight = window.innerHeight;
+        const windowHeight = window.innerHeight;
         // scroll location
-        let scrollTop = document.documentElement.scrollTop;
+        const scrollTop = document.documentElement.scrollTop;
         // all the files inside the window height
-        let docHeight = document.documentElement.offsetHeight;
+        const docHeight = document.documentElement.offsetHeight;
         if (Math.ceil(windowHeight + scrollTop) >= docHeight - offsetY) {
             setEndScreen(true);
         }
